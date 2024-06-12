a4531ef1e7699e53227e5a9715e96a563c6a6389
munim <muhammadmunimoff330@gmail.com>
2024-06-12 18:07:52 +0500

    done till faqs

--- static/script/script.js
+++ static/script/script.js
@@ -4,3 +4,21 @@ window.addEventListener('scroll', () => {
       > 10
     )
 })
+
+// show/hide faq
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open');
+
+    // change the icon
+    const icon = faq.querySelector('.faq__icon i');
+    if (icon.className === 'fa fa-plus') {
+      icon.className = 'fa fa-minus'
+    } else {
+      icon.className = 'fa fa-plus'
+    }
+  })
+})
